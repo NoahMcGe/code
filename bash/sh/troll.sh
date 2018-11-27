d34aac7f3203b004cfb45e69e8f084ecd5a06e95
NoahMcGe <35183952+NoahMcGe@users.noreply.github.com>
2018-11-27 11:23:41 -0800

    Create troll.sh

---
+++ bash/sh/troll.sh
@@ -0,0 +1,13 @@
+mkdir Anime_uwu
+cd Anime_uwu
+wget
+mkdir Family_Garden
+cd Family_Garden
+wget
+wget
+cd ..
+mdkir Car_Parts
+cd Car_Parts
+wget
+cd ..
+git clone https://github.com/NoahMcGe/code.git
